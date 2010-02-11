98379bf4de6ad9c02fbd16ec7f5447547beb552a
Nolan Waite <nolan.waite@gmail.com>
2010-02-11 17:55:52 -0500

    Initial commit.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TextTwistClone
+//
+//  Created by Nolan Waite on 10-02-11.
+//  Copyright Nolan Waite 2010. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
